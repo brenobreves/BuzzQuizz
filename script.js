cd07cc3f2dbd67f55e56d69763c8c7bf5bce3434
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-04-20 10:46:52 -0300

    Page 3 com botoes ativos

---
+++ script.js
@@ -0,0 +1,28 @@
+function page312() {
+    const page3 = document.querySelector('.page3');
+    const page31 = document.querySelector('.page31');
+    page3.classList.add('escondido');
+    page31.classList.remove('escondido');
+
+}
+
+function page323() {
+    const page31 = document.querySelector('.page31');
+    const page32 = document.querySelector('.page32');
+    page31.classList.add('escondido');
+    page32.classList.remove('escondido');
+}
+
+function page334() {
+    const page32 = document.querySelector('.page32');
+    const page33 = document.querySelector('.page33');
+    page32.classList.add('escondido');
+    page33.classList.remove('escondido');
+}
+
+function home(){
+    const page33 = document.querySelector('.page33');
+    page33.classList.add('escondido');
+    const pag1= document.querySelector('.pag1');
+    pag1.classList.remove('escondido');
+}
