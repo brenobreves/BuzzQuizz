2bf20010cca31f63bda363440b85d4833357b81a
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-04-20 13:49:17 -0300

    rsao o com correcoes page 3

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+////// PAGE 3 ////
+let createquizz = {}
+
 //PAGE 3 Sair da página de opções básicas de Quizz e ir para perguntas//
 function page312() {
     const page3 = document.querySelector('.page3');
@@ -24,9 +29,49 @@ function page334() {
 }
 
 ///// PAGE 3 Quizz finalizado, voltar pra home ////
-function home(){
+function home() {
     const page33 = document.querySelector('.page33');
     page33.classList.add('escondido');
-    const pag1= document.querySelector('.pag1');
+    const pag1 = document.querySelector('.pag1');
     pag1.classList.remove('escondido');
 }
+
+////Libera pergunta 2 /////
+function questions32() {
+    const questmenu = document.querySelector('.q2');
+    const quest = document.querySelector('.qq2');
+    questmenu.classList.add('escondido');
+    quest.classList.remove('escondido');
+    
+}
+
+
+/////Libera pergunta 3 //////
+
+function questions33() {
+    const questmenu = document.querySelector('.q3');
+    const quest = document.querySelector('.qq3');
+    questmenu.classList.add('escondido');
+    quest.classList.remove('escondido');
+}
+
+
+
+
+function basicdata3() {
+    titlee = document.querySelector('quiztitle3').value;
+    img = document.querySelector('.quizimgurl3').value;
+    createquizz = {
+        title: titlee,
+        image: img,
+    }
+}
+
+function questions3() {
+    numdep = document.querySelector('quiznump3').value;
+    let questions = "";
+
+}
+
+
+////// PAGE 3 FIM ////
