d03acbfe67a6afbed7bd70035278e0b7bbdb05d1
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-04-23 20:36:06 -0300

    Teste com criador de quizz

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+///variaveis globais page 3 ///
+let createquizz = {};
+createquizz.questions = [];
+createquizz.levels = [];
+//////////////////////
 ////// TOKEN //////
 axios.defaults.headers.common['Authorization'] = '9maaDDkKFQ1saSPY3udlpWmT';
 
@@ -349,10 +354,7 @@ function voltarHome() {
 
 
 ////// PAGE 3 ////
-let createquizz = {}
-createquizz.questions = [];
-createquizz.questions.answers = [];
-createquizz.levels = []
+
 
 //PAGE 3 Sair da página de opções básicas de Quizz e ir para perguntas//
 function page312() {
@@ -360,29 +362,51 @@ function page312() {
     const page31 = document.querySelector('.page31');
     page3.classList.add('escondido');
     page31.classList.remove('escondido');
-    basicdata3();
     window.scrollTo(0, 0);
-
 }
 /////
 ///PAGE 3 Sair da página de criar perguntas  e  ir para níveis///
 function page323() {
-    const page31 = document.querySelector('.page31');
-    const page32 = document.querySelector('.page32');
-    page31.classList.add('escondido');
-    page32.classList.remove('escondido');
-    window.scrollTo(0, 0);
-    questions3();
+    let numques = document.querySelector('.quiznump3').value;
+    let boxq = document.querySelector('.box31');
+    let titlee = boxq.querySelector('.qtitle').value;
+    let cor = boxq.querySelector('.qcolor').value;
+    const regex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+    if (titlee != null && regex.test(cor)) {
+        questions3()
+    }
+    else { }
 }
-/////
-///PAGE 3 Sair da página de Níveis e finalizar o quizz///
-function page334() {
+
+function troc() {
     const page32 = document.querySelector('.page32');
     const page33 = document.querySelector('.page33');
     page32.classList.add('escondido');
     page33.classList.remove('escondido');
     window.scrollTo(0, 0);
-    levels3();
+    questions3();
+}
+/////
+///PAGE 3 Sair da página de Níveis e finalizar o quizz///
+function qpronto() {
+    let promisse = axios.post('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes', {
+        title: createquizz.title,
+        image: createquizz.image,
+        questions: createquizz.questions,
+        levels: createquizz.levels
+    });
+    console.log(promisse);
+    promisse.then(page334());
+    promisse.catch(alert('server indisponivel'))
+}
+
+function page334() {
+
+    const page32 = document.querySelector('.page32');
+    const page33 = document.querySelector('.page33');
+    page32.classList.add('escondido');
+    page33.classList.remove('escondido');
+    window.scrollTo(0, 0);
 }
 //////
 ///// PAGE 3 Quizz finalizado, voltar pra home ////
@@ -412,84 +436,172 @@ function basicdata3() {
     let numlev = document.querySelector('.quizlev3').value;
     let box = document.querySelector('.box31');
     let box2 = document.querySelector('.box32')
-    for (i = 2; i <= numques; i++) {
-        box.innerHTML += '<div onclick="questions32(this)"  data-test="level-ctn" > <div class="questions312"> <div class="title312">Pergunta ' + i + '</div><ion-icon name="create-outline"></ion-icon> </div> <div class="questions31 escondido" id="' + i + '"> <div class="setor"> <div class="title31">Pergunta ' + i + '</div> <input type="text" class="qtitle" placeholder="Texto da pergunta"> <input type="text" class="qcolor" placeholder="Cor de fundo da pergunta"> </div> <div class="setor right"> <div class="title31">Resposta correta</div> <input type="text" class="qansw3" placeholder="Resposta correta"> <input type="text" class="qimg3" placeholder="URL da imagem"> </div> <div class="setor wrong1"> <div class="title31">Respostas incorretas</div> <input type="text" class="qansw3" placeholder="Resposta incorreta 1"> <input type="text" class="qimg3" placeholder="URL da imagem 1"> </div> <div class="setor wrong2"> <input type="text" class="qansw3" placeholder="Resposta incorreta 2"> <input type="text" class="qimg3" placeholder="URL da imagem 2"> </div> <div class="setor wrong3"> <input type="text" class="qansw3" placeholder="Resposta incorreta 3"> <input type="text" class="qimg3" placeholder="URL da imagem 3"> </div> </div>'
+    const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+    if (titlee.length >= 20 && titlee.length <= 65 && regex.test(img) && numques > 2 && numlev > 1) {
+        for (i = 2; i <= numques; i++) {
+            box.innerHTML += '<div onclick="questions32(this)"> <div class="questions312"> <div class="title312">Pergunta ' + i + '</div><ion-icon name="create-outline"></ion-icon> </div> <div class="questions31 escondido" id="' + i + '"> <div class="setor"> <div class="title31">Pergunta ' + i + '</div> <input type="text" class="qtitle" placeholder="Texto da pergunta"> <input type="text" class="qcolor" placeholder="Cor de fundo da pergunta"> </div> <div class="setor right"> <div class="title31">Resposta correta</div> <input type="text" class="qansw3" placeholder="Resposta correta"> <input type="text" class="qimg3" placeholder="URL da imagem"> </div> <div class="setor wrong1"> <div class="title31">Respostas incorretas</div> <input type="text" class="qansw3" placeholder="Resposta incorreta 1"> <input type="text" class="qimg3" placeholder="URL da imagem 1"> </div> <div class="setor wrong2"> <input type="text" class="qansw3" placeholder="Resposta incorreta 2"> <input type="text" class="qimg3" placeholder="URL da imagem 2"> </div> <div class="setor wrong3"> <input type="text" class="qansw3" placeholder="Resposta incorreta 3"> <input type="text" class="qimg3" placeholder="URL da imagem 3"> </div> </div>'
+        }
+        box.innerHTML += '<button onclick="page323()" class="proceed3">Prosseguir pra criar níveis</button>'
+        for (i = 2; i <= numlev; i++) {
+            box2.innerHTML += '<div onclick="questions32(this)"> <div class= "questions312"> <div class="title312">Nível ' + i + '</div><ion-icon name="create-outline"></ion-icon> </div> <div class="questions31 escondido" id="1' + i + '"> <div class="setor"> <div class="title31">Nível ' + i + '</div> <input type="text" class="levtitle" placeholder="Título do nível"> <input type="text" class="acertitle" placeholder="% de acerto mínima"> <input type="text" class="levimg" placeholder="URL da imagem do nível"> <input type="text" class="levdesc" placeholder="Descrição do nível"> </div> </div>';
+        }
+        box2.innerHTML += '<button onclick="levels3()" class="proceed3">Finalizar Quizz</button>'
+        createquizz.title = titlee;
+        createquizz.image = img;
+        page312();
     }
-    box.innerHTML += '<button onclick="page323()" class="proceed3">Prosseguir pra criar níveis</button>'
-    for (i = 2; i <= numlev; i++) {
-        box2.innerHTML += '<div onclick="questions32(this)"> <div class= "questions312"> <div class="title312">Nível ' + i + '</div><ion-icon name="create-outline"></ion-icon> </div> <div class="questions31 escondido" id="1' + i + '"> <div class="setor"> <div class="title31">Nível ' + i + '</div> <input type="text" class="levtitle" placeholder="Título do nível"> <input type="text" class="acertitle" placeholder="% de acerto mínima"> <input type="text" class="levimg" placeholder="URL da imagem do nível"> <input type="text" class="levdesc" placeholder="Descrição do nível"> </div> </div>';
-    }
-    box2.innerHTML += '<button onclick="page334()" class="proceed3">Finalizar Quizz</button>'
-    createquizz.title = titlee;
-    createquizz.image = img;
+    else { alert('siga os requisitos corretos') }
 }
 
 function questions3() {
     let numques = document.querySelector('.quiznump3').value;
     let boxq = document.querySelector('.box31');
-    let titlee = boxq.querySelector('.qtitle').value;
-    let cor = boxq.querySelector('.qcolor').value;
-    createquizz.questions.title = titlee;
-    createquizz.questions.color = cor;
-
-    let answers = [];
-    for (i = 1; i <= numques; i++) {
-        let question = document.getElementById(i);
+    const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+    for (i = 0; i < numques; i++) {
+        let testador = true;
+        let question = document.getElementById(i + 1);
+        let titlee = question.querySelector('.qtitle').value;
+        let cor = question.querySelector('.qcolor').value;
         let asw1 = question.querySelector('.right');
         let qasw1 = asw1.querySelector('.qansw3').value;
+        if (qasw1 = '') {
+            createquizz.questions[i].answers = [];
+            alert('deuruim');
+            testador = false;
+            break;
+        } else { };
         let iasw1 = asw1.querySelector('.qimg3').value;
+        if (regex.test(iasw1)) { } else {
+            createquizz.questions[i].answers = [];
+            alert('deuruim');
+            testador = false;
+            break;
+        };
+
+
+
 
         let asw2 = question.querySelector('.wrong1');
-        let qasw2 = asw1.querySelector('.qansw3').value;
-        let iasw2 = asw1.querySelector('.qimg3').value;
+        let qasw2 = asw2.querySelector('.qansw3').value;
+        if (qasw2 = '') {
+            createquizz.questions[i].answers = [];
+            alert('deuruim');
+            testador = false;
+            break;
+        } else { };
+        let iasw2 = asw2.querySelector('.qimg3').value;
+        if (regex.test(iasw2)) { } else {
+            createquizz.questions[i].answers = [];
+            alert('deuruim');
+            testador = false;
+            break;
+        }
+
+
 
         let asw3 = question.querySelector('.wrong2');
-        let qasw3 = asw1.querySelector('.qansw3').value;
-        let iasw3 = asw1.querySelector('.qimg3').value;
+        let qasw3 = asw3.querySelector('.qansw3').value;
+        let iasw3 = asw3.querySelector('.qimg3').value;
+        if (qasw3 != '') {
+            if (regex.test(iasw3)) { } else {
+                createquizz.questions[i].answers = [];
+                alert('deuruim');
+                testador = false;
+                break;
+            }
+        }
+
+
 
         let asw4 = question.querySelector('.wrong3');
-        let qasw4 = asw1.querySelector('.qansw3').value;
-        let iasw4 = asw1.querySelector('.qimg3').value;
-        let resposta = {}
-        resposta.text = qasw1;
-        resposta.image = iasw1;
-        resposta.iscorrect = true;
-        createquizz.questions.answers[0] = resposta;
-        resposta.text = qasw2;
-        resposta.image = iasw2;
-        resposta.iscorrect = false;
-        createquizz.questions.answers[1] = resposta;
-        resposta.text = qasw3;
-        resposta.image = iasw3;
-        resposta.iscorrect = false;
-        createquizz.questions.answers[2] = resposta;
-        resposta.text = qasw4;
-        resposta.image = iasw4;
-        resposta.iscorrect = false;
-        createquizz.questions.answers[3] = resposta;
-        let questinfo = {};
-        questinfo.title = titlee;
-        questinfo.color = cor;
+        let qasw4 = asw4.querySelector('.qansw3').value;
+        let iasw4 = asw4.querySelector('.qimg3').value;
+        if (qasw4 != '') {
+            if (regex.test(iasw4)) { } else {
+                createquizz.questions[i].answers = [];
+                alert('deuruim');
+                testador = false;
 
+                break;
+            }
+        }
+
+        createquizz.questions.push({
+            title: titlee,
+            color: cor,
+            answers: []
+        });
+
+        createquizz.questions[i].answers.push({
+            text: qasw1,
+            image: iasw1,
+            iscorrect: true
+        });
+
+        createquizz.questions[i].answers.push({
+            text: qasw2,
+            image: iasw2,
+            iscorrect: false
+        });
+
+        createquizz.questions[i].answers.push({
+            text: qasw3,
+            image: iasw3,
+            iscorrect: false
+        });
+
+        createquizz.questions[i].answers.push({
+            text: qasw4,
+            image: iasw4,
+            iscorrect: false
+        });
+        console.log('1');
+    }
+    if (testador = true ){
+        const page33 = document.querySelector('.page31');
+        page33.classList.add('escondido');
+        const pag1 = document.querySelector('.page32');
+        pag1.classList.remove('escondido');
+        window.scrollTo(0, 0);
 
     }
 }
 
 function levels3() {
     let numlev = document.querySelector('.quizlev3').value;
+    const regex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
     for (i = 1; i <= numlev; i++) {
         let level = document.getElementById(i + 10);
         let levtitlee = level.querySelector('.levtitle').value;
         let levporc = level.querySelector('.acertitle').value;
         let levimg = level.querySelector('.levimg').value;
         let levdesc = level.querySelector('.levdesc').value;
-        let resposta = {}
+        let resposta = {};
+        if (levtitlee < 10) {
+            createquizz.levels = [];
+            alert('deuruim');
+            break;
+        }
         resposta.title = levtitlee;
         resposta.image = levimg;
+        if (regex.test(levimg)) { } else {
+            createquizz.levels = [];
+            alert('deuruim');
+            break;
+        }
         resposta.text = levdesc;
+        if (levporc > 0 && levporc < 100) { }
+        else {
+            createquizz.levels = [];
+            alert('deuruim');
+            break;
+
+        }
         resposta.minValue = levporc;
-        createquizz.levels[i] = resposta;
+        createquizz.levels[i - 1] = resposta;
+        if (i = numlev) {
+            page334();
+        }
     }
-    console.log(createquizz);
 }
 ////// PAGE 3 FIM ///
